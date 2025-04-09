db2a0d5a6e0cebaf6babcbbb9928ba2a770e9712
MJ <themjtechnique@gmail.com>
2025-04-09 00:59:45 -0500

    Subiendo2

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function saludar() {
+    var campoNombre = document.getElementById("nombre");
+    var nombre = campoNombre.value;
+    if (nombre !== "") {
+      console.log("¡Hola, " + nombre + "! Gracias por estar aquí");
+    } 
+    
+    else {
+      console.log("Por favor, escribe tu nombre.");
+    }
+  }
+  
